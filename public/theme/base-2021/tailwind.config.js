90feb1bff0089d210bd7e755747825df60042d4e
LeoHugues <leo.hugues@hotmail.fr>
2021-01-30 11:21:02 +0100

    asset config

---
+++ public/theme/base-2021/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'vert-ciel': '#9ed8b0',
+                'sable': '#eddeab',
+                'rouge':'#87200c',
+                'sombre': '#160402'
+            },
+        }
+    },
+}
