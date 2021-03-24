a74491c3ed59b15cb030d0945c8bff9c9689decd
LeoHugues <leo.hugues@hotmail.fr>
2021-03-24 10:37:08 +0100

    create own theme folder

---
+++ public/theme/oc-aventures-2021/tailwind.config.js
@@ -0,0 +1,54 @@
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                'vert-ciel':{
+                    DEFAULT: '#9ED8B0',
+                    '50': '#FFFFFF',
+                    '100': '#FFFFFF',
+                    '200': '#FFFFFF',
+                    '300': '#E7F5EB',
+                    '400': '#C2E7CE',
+                    '500': '#9ED8B0',
+                    '600': '#7AC992',
+                    '700': '#55BB75',
+                    '800': '#3F9E5D',
+                    '900': '#317947'
+                },
+                'sable': {
+                    DEFAULT: '#EDDEAB',
+                    '50': '#FFFFFF',
+                    '100': '#FFFFFF',
+                    '200': '#FFFFFF',
+                    '300': '#FFFFFF',
+                    '400': '#F6EFD5',
+                    '500': '#EDDEAB',
+                    '600': '#E4CE81',
+                    '700': '#DBBD57',
+                    '800': '#D2AD2D',
+                    '900': '#A88A24'
+                },
+                'rouge':{
+                    DEFAULT: '#87200C',
+                    '50': '#F49784',
+                    '100': '#F2836D',
+                    '200': '#EE5B3E',
+                    '300': '#E53614',
+                    '400': '#B62B10',
+                    '500': '#87200C',
+                    '600': '#581508',
+                    '700': '#290A04',
+                    '800': '#160402',
+                  },
+                'sombre': '#222',
+            },
+            fontFamily: {
+                sans: ['Raleway', 'sans-serif'],
+                serif: ['Raleway', 'serif'],
+            },
+            screens: {
+                'lg2': '1040px',
+              },
+        },
+    }
+}
